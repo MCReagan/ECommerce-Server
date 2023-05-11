e434cc8a6853e828c26f6c713b5b8ec6b519dee0
MCReagan <127553558+MCReagan@users.noreply.github.com>
2023-05-10 20:14:19 -0500

    fixed class models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -29,7 +29,8 @@ Product.init(
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
-       validate: {
+      defaultValue: 10,
+      validate: {
         isNumeric: true,
       },
     },
